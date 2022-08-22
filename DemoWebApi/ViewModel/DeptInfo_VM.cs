2f2ac604f9363c6ac41fe59e9e38d028097df64e
Mahalakshmi08 <thevar.mahalakshmi18@siesgst.ac.in>
2022-08-22 15:23:36 +0530

    Implemented ViewModel with keys

--- DemoWebApi/ViewModel/DeptInfo_VM.cs
+++ DemoWebApi/ViewModel/DeptInfo_VM.cs
@@ -1,10 +1,13 @@
-﻿namespace DemoWebApi.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoWebApi.ViewModel
 {
     public class DeptInfo_VM
     {
+        [Key]
         public string Name { get; set; }
-        public int Count { get; set; }
-        public int TotalSalary { get; set; }
+        public int ?Count { get; set; }
+        public int ?TotalSalary { get; set; }
 
 
     }
