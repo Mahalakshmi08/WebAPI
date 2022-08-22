a1a9b078d0d8db9846e0e5a533cae7d858ce6018
Mahalakshmi08 <thevar.mahalakshmi18@siesgst.ac.in>
2022-08-22 14:51:30 +0530

    Tested basic get and post method

---
+++ DemoWebApi/Controllers/DeptController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DemoWebApi.Models;
+using System.Linq;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeptController : ControllerBase
+    {
+        db1045Context db = new db1045Context();
+
+        [HttpGet]
+        [Route("ListDept")]
+
+        public IActionResult GetDept()
+        {
+            //var data = db.Depts.ToList();
+            //var data = from dept in db.Depts select dept;
+            var data = from dept in db.Depts select new {Id=dept.Id, Name=dept.Name,Location = dept.Location};
+            return Ok(data);             //return code 200:ok
+
+        }
+
+
+        [HttpGet]
+        [Route("ListDept/{id}")]
+
+        public IActionResult GetDept(int?id)
+        {
+            if(id==null)
+            {
+                return BadRequest("Id Cannot be null");
+            }
+            //var data = db.Depts.Where(d => d.Id == id).Select(d => new {id = d.Id, Name=d.Name, Location=d.Location}).FirstOrDefault();
+            var data = (from dept in db.Depts where dept.Id == id select new {Id=dept.Id,Name = dept.Name, Location=dept.Location}).FirstOrDefault();
+            
+            if(data==null)
+            {
+                return NotFound($"Department {id} not present");
+            }
+            return Ok(data);
+        }
+
+        //......./api/dept/listcity?city=mumbai
+        [HttpGet]
+        [Route("ListCity")]
+
+        public IActionResult GetCity([FromQuery] string city)
+        {
+            var data = db.Depts.Where(d => d.Location == city);
+
+            if (data == null)
+            {
+                return NotFound($"No Locations found in {city}");
+            }           
+            return Ok(data);
+        }
+    }
+}
